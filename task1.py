c5c3f5a6a865ef37898f67696416eec58267979d
ivario123 <ivar.jonsson18@gmail.com>
2023-04-14 16:02:44 +0200

    Skeleton visulization

--- task1.py
+++ task1.py
@@ -10,11 +10,14 @@ from scipy.spatial.transform import Rotation as R
 import copy
 import matplotlib.pyplot as plt
 from os import environ
+
+
 def suppress_qt_warnings():
     environ["QT_DEVICE_PIXEL_RATIO"] = "0"
     environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
     environ["QT_SCREEN_SCALE_FACTORS"] = "1"
     environ["QT_SCALE_FACTOR"] = "1"
+
 
 if __name__ == "__main__":
     suppress_qt_warnings()
@@ -52,66 +55,72 @@ lookup = [
     "you",
 ]
 
+
 joints = [
-    "Hip_Center",
-    "Spine",
-    "Shoulder_Center",
     "Head",
+    "Shoulder_Center",
     "Shoulder_Left",
-    "Elbow_Left",
-    "Wrist_Left",
-    "Hand_Left",
     "Shoulder_Right",
+    "Elbow_Left",
     "Elbow_Right",
+    "Wrist_Left",
     "Wrist_Right",
+    "Hand_Left",
     "Hand_Right",
+    "Spine",
+    "Hip_Center",
     "Hip_Left",
-    "Knee_Left",
-    "Ankle_Left",
-    "Foot_Left",
     "Hip_Right",
+    "Knee_Left",
     "Knee_Right",
+    "Ankle_Left",
     "Ankle_Right",
+    "Foot_Left",
     "Foot_Right",
 ]
-joints_new = [
-    "Head", # Head
-    "Shoulder_Center", # Shoulder_Center
-    "Shoulder_Left", # Shoulder_Left
-    "Shoulder_Right", #  Shoulder_Right
-    "Elbow_Left", # Elbow_Left
-    "Elbow_Right",
-    "Wrist_Left", 
-    "Wrist_Right", # Wrist_Right ? 
-    "Hand_Left",
-    "Hand_Right", # Hand_Right ?
-    "Spine", # Spine
-    "Hip_Center",  # Hip_Center
-    "Hip_Left", # Hip_Left
-    "Hip_Right", # Hip_Right
-    "Knee_Left", # Knee_Left
-    "Knee_Right", # Knee_Right
-    "Ankle_Left", # Ankle_Left
-    "Ankle_Right", # Ankle_Right
-    "Foot_Left", # Foot_Left
-    "Foot_Right", # Foot_Right
-]
-print(len(joints))
-
-xyz = "xyz"
-angle = ["phi", "theta", "epsilon"]
 
 
-lables = ",".join(
-    [
-        ",".join([f"{joints_new[(i)//3]} {xyz[i%3]}" for i in range(0, 60)]),
-        ",".join([f"{joints_new[(i)//3]} {angle[i%3]}" for i in range(0, 60)]),
-        ",".join([f"mean {joints_new[(i)//3]} {angle[i%3]}" for i in range(0, 60)]),
-        ",".join([f"std {joints_new[(i)//3]} {angle[i%3]}" for i in range(0, 60)]),
-    ]
-)
-#print(lables)
-#exit(0)
+parent_joints = {
+    "Head": None,
+    "Shoulder_Center": "Head",
+    "Shoulder_Left": "Shoulder_Center",
+    "Shoulder_Right": "Shoulder_Center",
+    "Elbow_Left": "Shoulder_Left",
+    "Elbow_Right": "Shoulder_Right",
+    "Wrist_Left": "Elbow_Left",
+    "Wrist_Right": "Elbow_Right",
+    "Hand_Left": "Wrist_Left",
+    "Hand_Right": "Wrist_Right",
+    "Spine": "Shoulder_Center",
+    "Hip_Center": "Spine",
+    "Hip_Left": "Hip_Center",
+    "Hip_Right": "Hip_Center",
+    "Knee_Left": "Hip_Left",
+    "Knee_Right": "Hip_Right",
+    "Ankle_Left": "Knee_Left",
+    "Ankle_Right": "Knee_Right",
+    "Foot_Left": "Ankle_Left",
+    "Foot_Right": "Ankle_Right",
+}
+
+
+def render_labels():
+    xyz = "xyz"
+    angle = ["phi", "theta", "epsilon"]
+
+    lables = ",".join(
+        [
+            ",".join([f"{joints[(i)//3]} {xyz[i%3]}" for i in range(0, 60)]),
+            ",".join([f"{joints[(i)//3]} {angle[i%3]}" for i in range(0, 60)]),
+            ",".join([f"mean {joints[(i)//3]} {angle[i%3]}" for i in range(0, 60)]),
+            ",".join([f"std {joints[(i)//3]} {angle[i%3]}" for i in range(0, 60)]),
+        ]
+    )
+    print(lables)
+    exit(0)
+
+
+# render_labels()
 
 
 df = pandas.read_csv("train-final.csv")
@@ -153,6 +162,7 @@ class Gesture:
     def __init__(self, name, joints: list[Joint]):
         self.name = name
         self.joints = joints
+        self.joints_hash = {joint.name: joint for joint in self.joints}
         self.pcl = None
         self.angles = []
         self.coords = []
@@ -188,7 +198,7 @@ class Gesture:
         cl: np.ndarray = ls - spine
         cr: np.ndarray = rs - spine
         n = (1 / (mag(cl) * mag(cr))) * np.cross(cl, cr)
-        k = np.array([0, 0, 1])
+        k = np.array([0, 1, 0])
         phi = np.dot(n, k) / (mag(n))
         self.rotation = -phi
 
@@ -205,18 +215,39 @@ class Gesture:
         self.positions = []
         angles = []
         colors = []
+        indices = []
         for joint in self.joints:
+
+            # Add the joint to the point cloud
             self.positions.append(joint.xyz)
             angles.append(joint.ang)
-            color = (joints.index(joint.name) + 1) / 21
-            # print(color)
-            colors.append([color, color, color])
+            index = lookup.index(self.name)
+
+            # Add some color to the point cloud
+            color = (index + 1) / (len(lookup) + 1)
+            color_joint = (joints.index(joint.name) + 1) / (len(joints) + 1)
+            colors.append([color, 0, 1 - color])
+
+            # Draw a line to the parent joint
+            if joint.name in parent_joints.keys():
+                parent = parent_joints[joint.name]
+                if not parent:
+                    continue
+
+                indices.append([joints.index(joint.name), joints.index(parent)])
+
         self.angles = angles
         self.coords = self.positions
         pcd = o3d.geometry.PointCloud()
         pcd.points = o3d.utility.Vector3dVector(self.positions)
         pcd.colors = o3d.utility.Vector3dVector(colors)
+
+        ls = o3d.geometry.LineSet()
+        ls.points = o3d.utility.Vector3dVector(self.positions)
+        ls.lines = o3d.utility.Vector2iVector(indices)
+        ls.colors = o3d.utility.Vector3dVector(colors)
         self.pcl = pcd
+        self.ls = ls
 
     def render_pcl(self):
         if not self.pcl:
@@ -226,17 +257,15 @@ class Gesture:
     def matplot(self):
         fig = plt.figure()
         sbplt = fig.add_subplot(projection="3d")
-        sbplt.set_xlabel('X Label')
-        sbplt.set_ylabel('Y Label')
-        sbplt.set_zlabel('Z Label')
-
+        sbplt.set_xlabel("X")
+        sbplt.set_ylabel("Y")
+        sbplt.set_zlabel("Z")
         for joint in self.joints:
             xyz = joint.xyz
-            sbplt.scatter(*xyz,marker='o')
-            sbplt.text(*xyz,joint.name)
+            sbplt.scatter(*xyz, marker="o")
+            sbplt.text(*xyz, joint.name)
 
         plt.show()
-            
 
 
 def pack(data: pandas.DataFrame) -> list[Gesture]:
@@ -252,20 +281,30 @@ def pack(data: pandas.DataFrame) -> list[Gesture]:
             joint = Joint(joint, xyz, ang, mean, std)
 
             gesture.append(joint)
-        gestures.append(Gesture(gesture[0], gesture[1:]))
-        gestures[-1].norm_pos()
-        gestures[-1].correct_rotation()
-        gestures[-1].to_pcl()
+        gesture = Gesture(gesture[0], gesture[1:])
+        gesture.norm_pos()
+        gesture.correct_rotation()
+        gesture.to_pcl()
+        gestures.append(gesture)
 
     return gestures
 
 
 df = df.sort_values(by=["gesture id"])
-print(df)
+
 gestures = pack(df)
+
 
 print(len(gestures))
 FOR = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1 / 10, origin=[0, 0, 0])
-print("before viz")
-visualization.draw_geometries([FOR, *[gesture.pcl for gesture in gestures[0:-1]]])
-#gestures[-1].matplot()
+
+nmbr_of_gestures = 2
+
+visualization.draw_geometries(
+    [
+        FOR,
+        *[gesture.pcl for gesture in gestures[0:nmbr_of_gestures]],
+        *[gesture.ls for gesture in gestures[0:nmbr_of_gestures]],
+    ]
+)
+# gestures[-1].matplot()
